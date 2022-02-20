a407cc76cbff4d9eb1a4a6e22b3a76c824c93f40
Ethan Cemer <tylercemer@gmail.com>
2022-02-20 11:03:05 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './frontend/components/App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <React.StrictMode>
